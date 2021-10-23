4572262e109b2309e00db3fa49f5d01d52ba7a6c
Coper git <mosangini.matteo@liceocopernico.org>
2021-10-23 14:46:46 +0200

    New scripts dir

---
+++ scripts/power/wol_windows/run_script.cmd
@@ -0,0 +1,3 @@
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Set-ExecutionPolicy unrestricted"
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit  %~dp0\Enable-WOLWindowsNICSettings.ps1
