2bd15563e82cf1328fbae60cc6cd2669848aba71
Coper git <mosangini.matteo@liceocopernico.org>
2021-10-23 14:44:48 +0200

    Removed binary files from scripts dir

--- dist_files/power/wol_windows/run_script.cmd
+++
@@ -1,3 +0,0 @@
-
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Set-ExecutionPolicy unrestricted"
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit  %~dp0\Enable-WOLWindowsNICSettings.ps1
